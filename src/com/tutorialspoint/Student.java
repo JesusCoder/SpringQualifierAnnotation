36473260468f4cb2e0612f8fbca3e234ba1d0bda
Steve Sun <jiahuan@amazon.com>
2015-10-25 21:56:29 -0700

    use Lombok

--- src/com/tutorialspoint/Student.java
+++ src/com/tutorialspoint/Student.java
@@ -4,23 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class Student {
+	@Getter @Setter
 	private Integer age;
 	
+	@Getter @Setter
 	private String name;
 
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
 }
